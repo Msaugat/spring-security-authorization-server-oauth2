d177e16c6f159589b303d90603e93d8e33834c51
Msaugat <smudbhari85@gmail.com>
2026-05-05 23:55:12 +0545

    .

--- src/main/java/com/sec/service/MyUserDetailsService.java
+++ src/main/java/com/sec/service/MyUserDetailsService.java
@@ -8,8 +8,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.security.Provider;
-
 @Service
 @RequiredArgsConstructor
 public class MyUserDetailsService implements UserDetailsService {
